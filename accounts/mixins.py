af7e8b1651727fd482d434d2aa0b6368ea761117
johnbenedictyan <johnyfx00@gmail.com>
2020-10-31 00:11:15 +0800

    Intialised the accounts mixins file.

---
+++ accounts/mixins.py
@@ -0,0 +1,18 @@
+# Imports from python
+
+# Imports from django
+
+# Imports from other apps
+
+# Imports from within the app
+from .models import Employer
+
+# Utiliy Classes and Functions
+
+# Start of Mixins
+
+class EmployerVerifiedMixin:
+    def get_object(self, queryset=None):
+        return Employer.objects.get(
+            pk = self.request.user.pk
+    )
