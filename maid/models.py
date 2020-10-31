504abaac0caeb190d645005f8710771016af5231
johnbenedictyan <johnyfx00@gmail.com>
2020-10-31 12:14:19 +0800

    Initalised the maid models file.

--- maid/models.py
+++ maid/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+# Imports from python
 
-# Create your models here.
+# Imports from django
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator, URLValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
+
+# Imports from other apps
+
+# Imports from within the app
+
+# Utiliy Classes and Functions
+
+# Start of Models
