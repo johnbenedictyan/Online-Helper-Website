ece88f6b70ae1ac9048a23db7f0da1e93092d475
johnbenedictyan <johnyfx00@gmail.com>
2020-10-31 23:33:13 +0800

    Removed the unnecssary mixin.

--- agency/mixins.py
+++ agency/mixins.py
@@ -5,14 +5,7 @@
 # Imports from other apps
 
 # Imports from within the app
-from .models import Agency
 
 # Utiliy Classes and Functions
 
 # Start of Mixins
-
-class AgencyVerifiedMixin:
-    def get_object(self, queryset=None):
-        return Agency.objects.get(
-            pk = self.request.user.pk
-    )
