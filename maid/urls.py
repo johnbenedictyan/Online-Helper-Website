1c1839f0b485184c08d0cd387a6d39ef5ae193bd
johnbenedictyan <johnyfx00@gmail.com>
2020-10-31 23:38:03 +0800

    Initialised the maid app's views and their respective urls.

--- maid/urls.py
+++ maid/urls.py
@@ -4,24 +4,23 @@ from django.urls import include, path
 # Imports from foreign installed apps
 
 # Imports from local app
-# from .forms import (
-    
-# )
 
 ## Create Views
-# from .views import (
-        
-# )
+from .views import (
+    MaidCreate
+)
 
 ## Update Views
-# from .views import (
-    
-# )
+from .views import (
+    MaidUpdate, MaidBiodataUpdate, MaidFamilyDetailsUpdate, 
+    MaidInfantChildCareUpdate, MaidElderlyCareUpdate, MaidDisabledCareUpdate,
+    MaidGeneralHouseworkUpdate,MaidCookingUpdate
+)
 
 ## Delete Views
-# from .views import (
-#  
-# )
+from .views import (
+    MaidDelete
+)
 
 # Start of Urls
 
@@ -29,19 +28,66 @@ urlpatterns = [
     path(
         'create/',
         include([
-            
+            path(
+                '',
+                MaidCreate.as_view(),
+                name='maid_create'
+            )
         ])
     ),
     path(
         'delete/',
         include([
-
+            path(
+                '<int:pk>/',
+                MaidDelete.as_view(),
+                name='maid_delete'
+            ),
         ])
     ),
     path(
-        'update/',
+        'update/<int:pk>/',
         include([
-
+            path(
+                '',
+                MaidUpdate.as_view(),
+                name='maid_update'
+            ),
+            path(
+                'biodata/',
+                MaidBiodataUpdate.as_view(),
+                name='maid_biodata_update'
+            ),
+            path(
+                'family-details/',
+                MaidFamilyDetailsUpdate.as_view(),
+                name='maid_family_details_update'
+            ),
+            path(
+                'icc/',
+                MaidInfantChildCareUpdate.as_view(),
+                name='maid_infant_child_care_update'
+            ),
+            path(
+                'ec/',
+                MaidElderlyCareUpdate.as_view(),
+                name='maid_elderly_care_update'
+            ),
+            path(
+                'dc/',
+                MaidDisabledCareUpdate.as_view(),
+                name='maid_disabled_care_update'
+            ),
+            path(
+                'gh/',
+                MaidGeneralHouseworkUpdate.as_view(),
+                name='maid_general_housework_update'
+            ),
+            path(
+                'c/',
+                MaidCookingUpdate.as_view(),
+                name='maid_cooking_update'
+            )
         ])
     )
 ]
--- maid/views.py
+++ maid/views.py
@@ -1,3 +1,208 @@
-from django.shortcuts import render
+# Imports from django
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.base import RedirectView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+# Imports from foreign installed apps
+from agency.models import Agency
+
+# Imports from local app
+from .forms import (
+    MaidCreationForm, MaidBiodataForm, MaidFamilyDetailsForm, 
+    MaidInfantChildCareForm, MaidElderlyCareForm, MaidDisabledCareForm,
+    MaidGeneralHouseworkForm, MaidCookingForm, MaidFoodHandlingPreferenceForm,
+    MaidDietaryRestrictionForm
+)
+
+from .models import (
+    Maid, MaidBiodata, MaidFamilyDetails, MaidInfantChildCare, MaidElderlyCare,
+    MaidDisabledCare, MaidGeneralHousework, MaidCooking, 
+    MaidFoodHandlingPreference, MaidDietaryRestriction
+)
+
+# Start of Views
+
+# Template Views
+
+# Redirect Views
+
+# Detail Views
+    
+# Create Views
+class MaidCreate(CreateView):
+    context_object_name = 'maid'
+    form_class = MaidCreationForm
+    http_method_names = ['get','post']
+    model = Maid
+    template_name = 'maid-create.html'
+    success_url = reverse_lazy('')
+
+    def form_valid(self, form):
+        form.instance.agency = self.request.user
+        response = super().form_valid(form)
+        MaidBiodata.objects.create(
+            maid=self.object
+        )
+        MaidFamilyDetails.objects.create(
+            maid=self.object
+        )
+        MaidInfantChildCare.objects.create(
+            maid=self.object
+        )
+        MaidElderlyCare.objects.create(
+            maid=self.object
+        )
+        MaidDisabledCare.objects.create(
+            maid=self.object
+        )
+        MaidGeneralHousework.objects.create(
+            maid=self.object
+        )
+        MaidCooking.objects.create(
+            maid=self.object
+        )
+
+# Update Views
+class MaidUpdate(LoginRequiredMixin, UpdateView):
+    context_object_name = 'maid'
+    form_class = MaidCreationForm
+    http_method_names = ['get','post']
+    model = Maid
+    template_name = 'maid-update.html'
+    success_url = reverse_lazy('')
+
+    def get_object(self, queryset=None):
+        return Maid.objects.get(
+            pk = self.kwargs.pk_url_kwarg,
+            agency = Agency.objects.get(
+                pk = self.request.user.pk
+            )
+        )
+
+class MaidBiodataUpdate(LoginRequiredMixin, UpdateView):
+    context_object_name = 'maid_biodata'
+    form_class = MaidBiodataForm
+    http_method_names = ['get','post']
+    model = MaidBiodata
+    template_name = 'maid-biodata-update.html'
+    success_url = reverse_lazy('')
+
+    def get_object(self, queryset=None):
+        return MaidBiodata.objects.get(
+            maid = Maid.objects.get(
+                pk = self.pk_url_kwarg
+            )
+        )
+
+class MaidFamilyDetailsUpdate(LoginRequiredMixin, UpdateView):
+    context_object_name = 'maid_family_details'
+    form_class = MaidFamilyDetailsForm
+    http_method_names = ['get','post']
+    model = MaidFamilyDetails
+    template_name = 'maid-family-details-update.html'
+    success_url = reverse_lazy('')
+
+    def get_object(self, queryset=None):
+        return MaidFamilyDetails.objects.get(
+            maid = Maid.objects.get(
+                pk = self.pk_url_kwarg
+            )
+        )
+
+class MaidInfantChildCareUpdate(LoginRequiredMixin, UpdateView):
+    context_object_name = 'maid_infant_child_care'
+    form_class = MaidInfantChildCareForm
+    http_method_names = ['get','post']
+    model = MaidInfantChildCare
+    template_name = 'maid-infant-child-care-update.html'
+    success_url = reverse_lazy('')
+
+    def get_object(self, queryset=None):
+        return MaidInfantChildCare.objects.get(
+            maid = Maid.objects.get(
+                pk = self.pk_url_kwarg
+            )
+        )
+
+class MaidElderlyCareUpdate(LoginRequiredMixin, UpdateView):
+    context_object_name = 'maid_elderly_care'
+    form_class = MaidElderlyCareForm
+    http_method_names = ['get','post']
+    model = MaidElderlyCare
+    template_name = 'maid-elderly-care-update.html'
+    success_url = reverse_lazy('')
+
+    def get_object(self, queryset=None):
+        return MaidElderlyCare.objects.get(
+            maid = Maid.objects.get(
+                pk = self.pk_url_kwarg
+            )
+        )
+
+class MaidDisabledCareUpdate(LoginRequiredMixin, UpdateView):
+    context_object_name = 'maid_disabled_care'
+    form_class = MaidDisabledCareForm
+    http_method_names = ['get','post']
+    model = MaidDisabledCare
+    template_name = 'maid-disabled-care-update.html'
+    success_url = reverse_lazy('')
+
+    def get_object(self, queryset=None):
+        return MaidDisabledCare.objects.get(
+            maid = Maid.objects.get(
+                pk = self.pk_url_kwarg
+            )
+        )
+
+class MaidGeneralHouseworkUpdate(LoginRequiredMixin, UpdateView):
+    context_object_name = 'maid_general_housework'
+    form_class = MaidGeneralHouseworkForm
+    http_method_names = ['get','post']
+    model = MaidGeneralHousework
+    template_name = 'maid-general-housework-update.html'
+    success_url = reverse_lazy('')
+
+    def get_object(self, queryset=None):
+        return MaidGeneralHousework.objects.get(
+            maid = Maid.objects.get(
+                pk = self.pk_url_kwarg
+            )
+        )
+
+class MaidCookingUpdate(LoginRequiredMixin, UpdateView):
+    context_object_name = 'maid_cooking'
+    form_class = MaidCookingForm
+    http_method_names = ['get','post']
+    model = MaidCooking
+    template_name = 'maid-cooking-update.html'
+    success_url = reverse_lazy('')
+
+    def get_object(self, queryset=None):
+        return MaidCooking.objects.get(
+            maid = Maid.objects.get(
+                pk = self.pk_url_kwarg
+            )
+        )
+
+# Delete Views
+class MaidDelete(LoginRequiredMixin, DeleteView):
+    context_object_name = 'maid'
+    http_method_names = ['get','post']
+    model = Maid
+    template_name = 'maid-delete.html'
+    success_url = reverse_lazy('')
+
+    def get_object(self, queryset=None):
+        return Maid.objects.get(
+            pk = self.kwargs.pk_url_kwarg,
+            agency = Agency.objects.get(
+                pk = self.request.user.pk
+            )
+        )
