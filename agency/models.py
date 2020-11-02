2ec93544ea7167276d5f608035694b246d174576
johnbenedictyan <johnyfx00@gmail.com>
2020-11-02 12:39:10 +0800

    Moved the advertisement models into their own advertisement app.

--- agency/models.py
+++ agency/models.py
@@ -16,8 +16,6 @@ from django.utils.translation import ugettext_lazy as _
 
 # Start of Models
 
-# User models
-
 class Agency(models.Model):
     user = models.OneToOneField(
         get_user_model(),
@@ -313,64 +311,6 @@ class AgencyOperatingHours(models.Model):
         verbose_name=_('Public holiday opening hours'),
         max_length=30,
         blank=True
-    )
-
-# Advertisement models
-class AdvertisementLocation(models.Model):
-    class AdvertisementTierChoices(models.TextChoices):
-        NONE = 'NONE', _('None')
-        TIER_1 = 'TIER1', _('Tier 1')
-        TIER_2 = 'TIER2', _('Tier 2')
-        TIER_3 = 'TIER3', _('Tier 3')
-
-    name = models.TextField(
-        verbose_name=_('Page name'),
-        max_length=30,
-        blank=False
-    )
-
-    tier = models.TextField(
-        verbose_name=_('Advertisment tier'),
-        max_length=5,
-        blank=False,
-        choices=AdvertisementTierChoices.choices,
-        default=AdvertisementTierChoices.NONE
-    )
-
-class Advertisement(models.Model):
-    class AdvertisementTypeChoices(models.TextChoices):
-        NONE = 'NONE', _('None')
-        BANNER = 'BANNER', _('Banner')
-        TESTIMONIAL = 'TESTI', _('Testimonial')
-
-    agency = models.ForeignKey(
-        Agency,
-        on_delete=models.CASCADE,
-        related_name='advertisements'
-    )
-
-    location = models.ForeignKey(
-        AdvertisementLocation,
-        on_delete=models.CASCADE,
-        related_name='advertisements'
-    )
-
-    ad_type = models.TextField(
-        verbose_name=_('Advertisment type'),
-        max_length=6,
-        blank=False,
-        choices=AdvertisementTypeChoices.choices,
-        default=AdvertisementTypeChoices.NONE
-    )
-
-    start_date = models.DateTimeField(
-        verbose_name=_('Advertisment start date'),
-        editable=False
-    )
-
-    start_date = models.DateTimeField(
-        verbose_name=_('Advertisment end date'),
-        editable=False
     )
 
 # Django Signals
