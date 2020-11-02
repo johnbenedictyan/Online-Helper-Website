66541ae4f619298a812b6d09f49d03d68cec2c5b
johnbenedictyan <johnyfx00@gmail.com>
2020-11-02 14:03:43 +0800

    Added the invoice list and detail views.

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+# Imports from django
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+# Imports from project-wide files
+
+# Imports from foreign installed apps
+
+# Imports from local app
+from .models import Invoice
+
+# Start of Views
+
+# Template Views
+
+# Redirect Views
+
+# List Views
+class InvoiceList(LoginRequiredMixin, ListView):
+    context_object_name = 'invoice'
+    http_method_names = ['get']
+    model = Invoice
+    template_name = 'invoice-list.html'
+
+    def get_queryset(self):
+        return Invoice.objects.filter(
+            agency__pk = self.request.user.pk
+        )
+
+# Detail Views
+class InvoiceDetail(LoginRequiredMixin, DetailView):
+    context_object_name = 'invoice'
+    http_method_names = ['get']
+    model = Invoice
+    template_name = 'invoice-detail.html'
+
+# Create Views
+
+# Delete Views
