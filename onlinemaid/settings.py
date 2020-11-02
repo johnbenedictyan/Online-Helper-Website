8412c99d89daaa90a841ed3c711f25d61deba427
johnbenedictyan <johnyfx00@gmail.com>
2020-11-02 12:46:03 +0800

    Added the apps into the installed apps list.

--- onlinemaid/settings.py
+++ onlinemaid/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'advertisement',
     'agency',
     'dashboard',
     'maid',
+    'shortlist',
+    'website',
     'crispy_forms'
 ]
 
