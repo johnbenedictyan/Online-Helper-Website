e9b5fa03cb2098ba8ed4256d6209fc69063b70fa
johnbenedictyan <johnyfx00@gmail.com>
2020-11-06 15:48:06 +0800

    Added the routes for the newly created website app views.

---
+++ website/urls.py
@@ -0,0 +1,46 @@
+# Imports from django
+from django.urls import include, path
+
+# Imports from foreign installed apps
+
+# Imports from local app
+
+## Redirect Views
+
+## Template Views 
+from .views import HomeView, AboutUsView, ContactUsView, TermsOfSerivceView
+
+## List Views
+
+## Detail Views
+
+## Create Views
+
+## Update Views
+
+## Delete Views
+
+# Start of Urls
+
+urlpatterns = [
+        path(
+            '',
+            HomeView.as_view(),
+            name='home'
+        ),
+        path(
+            'about-us/',
+            AboutUsView.as_view(),
+            name='about_us'
+        ),
+        path(
+            'contact-us/',
+            ContactUsView.as_view(),
+            name='contact_us'
+        ),
+        path(
+            'terms-of-service/',
+            TermsOfSerivceView.as_view(),
+            name='terms_of_service'
+        )
+]
