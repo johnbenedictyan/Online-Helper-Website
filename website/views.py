6502b95155b8b072a949162ed70b6bf08a2465f4
johnbenedictyan <johnyfx00@gmail.com>
2020-11-06 15:47:50 +0800

    Added the home, about us, contact us and terms of service views.

--- website/views.py
+++ website/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+# Imports from django
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+# Imports from project-wide files
+
+# Imports from foreign installed apps
+
+# Imports from local app
+
+# Start of Views
+
+# Template Views
+class HomeView(TemplateView):
+    http_method_names = ['get']
+    template_name = 'home.html'
+
+class AboutUsView(TemplateView):
+    http_method_names = ['get']
+    template_name = 'about-us.html'
+
+class ContactUsView(TemplateView):
+    http_method_names = ['get']
+    template_name = 'contact-us.html'
+
+class TermsOfSerivceView(TemplateView):
+    http_method_names = ['get']
+    template_name = 'terms-of-service.html'
+
+# Redirect Views
+
+# List Views
+
+# Detail Views
+
+# Create Views
+
+# Update Views
+
+# Delete Views
