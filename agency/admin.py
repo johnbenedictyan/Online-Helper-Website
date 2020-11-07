d52b19004c07634a4e735e7fcde9ffec88e12173
johnbenedictyan <johnyfx00@gmail.com>
2020-11-07 23:11:28 +0800

    Edited the agency admin file according to the change to agency branch.

--- agency/admin.py
+++ agency/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from .models import (
-    Agency, AgencyEmployee, AgencyContactInformation, AgencyLocation,
-    AgencyOperatingHours, AgencyPlan
+    Agency, AgencyEmployee, AgencyBranch, AgencyOperatingHours, AgencyPlan
 )
 
 # Register your models here.
 admin.site.register(Agency)
 admin.site.register(AgencyEmployee)
-admin.site.register(AgencyContactInformation)
-admin.site.register(AgencyLocation)
+admin.site.register(AgencyBranch)
 admin.site.register(AgencyOperatingHours)
 admin.site.register(AgencyPlan)
