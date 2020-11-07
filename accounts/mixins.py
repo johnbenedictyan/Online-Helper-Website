302cac2e12f2530b0b176132e307e40400102811
johnbenedictyan <johnyfx00@gmail.com>
2020-11-07 10:01:54 +0800

    Modified the verified employer mixin.

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -12,7 +12,16 @@ from .models import Employer
 # Start of Mixins
 
 class VerifiedEmployerMixin:
-    def get_object(self, queryset=None):
+    def check_employer(self):
         return Employer.objects.get(
             pk = self.request.user.pk
-    )
+        )
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.check_employer():
+            return self.handle_no_permission(request)
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        return self.check_employer()
