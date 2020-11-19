3f074a4715ab2258f0882e9d9baf2eb47fffdac7
johnbenedictyan <johnyfx00@gmail.com>
2020-11-19 21:23:15 +0800

    Added the superuserrequired mixin which raises a permission denied if the user is not a super user.

--- agency/mixins.py
+++ agency/mixins.py
@@ -1,6 +1,7 @@
 # Imports from python
 
 # Imports from django
+from django.contrib.auth.mixins import AccessMixin
 
 # Imports from other apps
 
@@ -9,3 +10,13 @@
 # Utiliy Classes and Functions
 
 # Start of Mixins
+
+class SuperUserRequiredMixin(AccessMixin):
+    permission_denied_message = '''You are required to login as a member of 
+                                Online Maid Pte Ltd to perform this action'''
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
