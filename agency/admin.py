2dd12ea7cf91be2eb270ad8f83aac52d6ae6a423
johnbenedictyan <johnyfx00@gmail.com>
2020-11-19 22:41:39 +0800

    Registered the agency owner and manager models inside of the agency admin.

--- agency/admin.py
+++ agency/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import (
-    Agency, AgencyEmployee, AgencyBranch, AgencyOperatingHours, AgencyPlan
+    Agency, AgencyEmployee, AgencyBranch, AgencyOperatingHours, AgencyPlan,
+    AgencyManager, AgencyOwner
 )
 
 # Register your models here.
@@ -9,3 +10,5 @@ admin.site.register(AgencyEmployee)
 admin.site.register(AgencyBranch)
 admin.site.register(AgencyOperatingHours)
 admin.site.register(AgencyPlan)
+admin.site.register(AgencyManager)
+admin.site.register(AgencyOwner)
