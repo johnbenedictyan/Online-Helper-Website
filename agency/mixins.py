a98f4a3b9b0e31bade21a5609711519b7b39fc79
johnbenedictyan <johnyfx00@gmail.com>
2020-11-21 10:02:40 +0800

    Added the custom group required mixins for group permissions.

--- agency/mixins.py
+++ agency/mixins.py
@@ -1,25 +1,24 @@
 # Imports from python
 
 # Imports from django
-from django.contrib.auth.mixins import AccessMixin, LoginRequiredMixin
+from onlinemaid.mixins import (
+    AccessMixin, LoginRequiredMixin, SuperUserRequiredMixin, GroupRequiredMixin
+)
+
 from django.urls import reverse_lazy
 
 # Imports from other apps
 
 # Imports from within the app
+from .models import Agency
 
 # Utiliy Classes and Functions
 
 # Start of Mixins
 
-class SuperUserRequiredMixin(AccessMixin):
+class OnlineMaidStaffRequiredMixin(SuperUserRequiredMixin):
     permission_denied_message = '''You are required to login as a member of 
                                 Online Maid Pte Ltd to perform this action'''
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_superuser:
-            return self.handle_no_permission()
-        return super().dispatch(request, *args, **kwargs)
 
 class AgencyLoginRequiredMixin(LoginRequiredMixin):
     login_url = reverse_lazy('agency_sign_in')
@@ -27,3 +26,41 @@ class AgencyLoginRequiredMixin(LoginRequiredMixin):
     # This mixin is the base mixin if we want the user to login in using the 
     # agency sign in page rather than the sign in page for the potential 
     # employers.
+
+class AgencyOwnerRequiredMixin(GroupRequiredMixin):
+    group_required = u"Agency Owners"
+    login_url = reverse_lazy('agency_sign_in')
+    permission_denied_message = '''You are required to login using an Agency
+                                owners account to perform this action'''
+                                
+
+class AgencyAdministratorRequiredMixin(GroupRequiredMixin):
+    group_required = u"Agency Administrators"
+    login_url = reverse_lazy('agency_sign_in')
+    permission_denied_message = '''You are required to login using an Agency
+                                administrator account to perform this 
+                                action'''
+
+class AgencyManagerRequiredMixin(GroupRequiredMixin):
+    group_required = u"Agency Managers"
+    login_url = reverse_lazy('agency_sign_in')
+    permission_denied_message = '''You are required to login using an Agency
+                                managers account to perform this action'''
+
+class AgencyAdminTeamRequiredMixin(GroupRequiredMixin):
+    group_required = [u"Agency Owners", u"Agency Administrators"]
+    login_url = reverse_lazy('agency_sign_in')
+    permission_denied_message = '''You are required to login using either
+                                an Agency owner or administrator account 
+                                to perform this action'''
+
+class AgencySalesTeamRequiredMixin(GroupRequiredMixin):
+    group_required = [
+        u"Agency Administrators",
+        u"Agency Managers",
+        u"Agency Sales Team"
+    ]
+    login_url = reverse_lazy('agency_sign_in')
+    permission_denied_message = '''You are required to login using either
+                                an Agency administrator, manager or sales staff
+                                account to perform this action'''
