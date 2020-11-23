6772910df98e7624d194eca769201434f74b4961
johnbenedictyan <johnyfx00@gmail.com>
2020-11-23 17:39:30 +0800

    Added the staticfiles directory setting.

--- onlinemaid/settings.py
+++ onlinemaid/settings.py
@@ -138,6 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, "static")
+]
 
 AUTH_USER_MODEL = 'accounts.User'
 
