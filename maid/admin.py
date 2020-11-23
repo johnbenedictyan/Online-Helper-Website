711c8f39fe5925a250f7324ee0a6edec85a3cd1d
johnbenedictyan <johnyfx00@gmail.com>
2020-11-23 18:26:10 +0800

    Registered all of the maid models in the maid app admin file.

--- maid/admin.py
+++ maid/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+    Maid, MaidWorkDuty, MaidFoodHandlingPreference, MaidDietaryRestriction,
+    MaidEmploymentHistory, MaidBiodata, MaidStatus, MaidFamilyDetails,
+    MaidInfantChildCare, MaidElderlyCare, MaidDisabledCare, 
+    MaidGeneralHousework, MaidCooking
+)
 
 # Register your models here.
+admin.site.register(Maid)
+admin.site.register(MaidWorkDuty)
+admin.site.register(MaidFoodHandlingPreference)
+admin.site.register(MaidDietaryRestriction)
+admin.site.register(MaidEmploymentHistory)
+admin.site.register(MaidBiodata)
+admin.site.register(MaidStatus)
+admin.site.register(MaidFamilyDetails)
+admin.site.register(MaidInfantChildCare)
+admin.site.register(MaidElderlyCare)
+admin.site.register(MaidDisabledCare)
+admin.site.register(MaidGeneralHousework)
+admin.site.register(MaidCooking)
