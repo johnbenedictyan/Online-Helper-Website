dab07740d124e49d25c4348f351fc340aeea5033
johnbenedictyan <johnyfx00@gmail.com>
2020-11-25 23:54:09 +0800

    Changed all of the django signals for the checking of each of the sub parts of the maid model.

--- maid/models.py
+++ maid/models.py
@@ -777,112 +777,122 @@ def maid_completed(maid):
         maid.save()
 
 @receiver(post_save, sender=MaidBiodata)
-def maid_biodata_completed(sender, instance, **kwargs):
-    maid = instance.maid
-    biodata_valid = True
+def maid_biodata_completed(sender, instance, created, **kwargs):
+    if created == False:
+        maid = instance.maid
+        biodata_valid = True
 
-    while biodata_valid == True:
-        for i in sender.values():
-            if not i:
+        for k,v in instance.__dict__.items():
+            if not v:
                 biodata_valid = False
-    
-    maid.biodata_complete = biodata_valid
-    maid.save()
-    maid_completed(maid)
+
+        maid.biodata_complete = biodata_valid
+        maid.save()
+        if biodata_valid == True:
+            maid_completed(maid)
 
 @receiver(post_save, sender=MaidFamilyDetails)
-def maid_family_details_completed(sender, instance, **kwargs):
-    maid = instance.maid
-    family_details_valid = True
-    
-    while family_details_valid == True:
-        for i in sender.values():
-            if not i:
+def maid_family_details_completed(sender, instance, created, **kwargs):
+    if created == False:
+        maid = instance.maid
+        family_details_valid = True
+
+        for k,v in instance.__dict__.items():
+            if not v:
                 family_details_valid = False
-    
-    maid.family_details_complete = family_details_valid
-    maid.save()
-    maid_completed(maid)
+        
+        maid.family_details_complete = family_details_valid
+        maid.save()
+        if family_details_valid == True:
+            maid_completed(maid)
 
 @receiver(post_save, sender=MaidInfantChildCare)
-def maid_infant_child_care_completed(sender, instance, **kwargs):
-    maid = instance.maid
-    infant_child_care_valid = True
-    
-    while infant_child_care_valid == True:
-        for i in sender.values():
-            if not i:
+def maid_infant_child_care_completed(sender, instance, created, **kwargs):
+    if created == False:
+        maid = instance.maid
+        infant_child_care_valid = True
+
+        for k,v in instance.__dict__.items():
+            if not v:
                 infant_child_care_valid = False
-    
-    maid.infant_child_care_complete = infant_child_care_valid
-    maid.save()
-    maid_completed(maid)
+        
+        maid.infant_child_care_complete = infant_child_care_valid
+        maid.save()
+        if infant_child_care_valid == True:
+            maid_completed(maid)
 
 @receiver(post_save, sender=MaidElderlyCare)
-def maid_elderly_care_completed(sender, instance, **kwargs):
-    maid = instance.maid
-    elderly_care_valid = True
-    
-    while elderly_care_valid == True:
-        for i in sender.values():
-            if not i:
+def maid_elderly_care_completed(sender, instance, created, **kwargs):
+    if created == False:
+        maid = instance.maid
+        elderly_care_valid = True
+
+        for k,v in instance.__dict__.items():
+            if not v:
                 elderly_care_valid = False
-    
-    maid.elderly_care_complete = elderly_care_valid
-    maid.save()
-    maid_completed(maid)
+        
+        maid.elderly_care_complete = elderly_care_valid
+        maid.save()
+        if elderly_care_valid == True:
+            maid_completed(maid)
 
 @receiver(post_save, sender=MaidDisabledCare)
-def maid_disabled_care_completed(sender, instance, **kwargs):
-    maid = instance.maid
-    disabled_care_valid = True
-    
-    while disabled_care_valid == True:
-        for i in sender.values():
-            if not i:
+def maid_disabled_care_completed(sender, instance, created, **kwargs):
+    if created == False:
+        maid = instance.maid
+        disabled_care_valid = True
+
+        for k,v in instance.__dict__.items():
+            if not v:
                 disabled_care_valid = False
     
-    maid.disabled_care_complete = disabled_care_valid
-    maid.save()
-    maid_completed(maid)
+        maid.disabled_care_complete = disabled_care_valid
+        maid.save()
+        if disabled_care_valid == True:
+            maid_completed(maid)
 
 @receiver(post_save, sender=MaidGeneralHousework)
-def maid_general_housework_completed(sender, instance, **kwargs):
-    maid = instance.maid
-    general_housework_valid = True
-    
-    while general_housework_valid == True:
-        for i in sender.values():
-            if not i:
+def maid_general_housework_completed(sender, instance, created, **kwargs):
+    if created == False:
+        maid = instance.maid
+        general_housework_valid = True
+
+        for k,v in instance.__dict__.items():
+            if not v:
                 general_housework_valid = False
-    
-    maid.general_housework_complete = general_housework_valid
-    maid.save()
-    maid_completed(maid)
+        
+        maid.general_housework_complete = general_housework_valid
+        maid.save()
+        if general_housework_valid == True:
+            maid_completed(maid)
 
 @receiver(post_save, sender=MaidCooking)
-def maid_cooking_completed(sender, instance, **kwargs):
-    maid = instance.maid
-    cooking_valid = True
-    
-    while cooking_valid == True:
-        for i in sender.values():
-            if not i:
+def maid_cooking_completed(sender, instance, created, **kwargs):
+    if created == False:
+        maid = instance.maid
+        cooking_valid = True
+
+        for k,v in instance.__dict__.items():
+            if not v:
                 cooking_valid = False
-    
-    maid.cooking_complete = cooking_valid
-    maid.save()
-    maid_completed(maid)
+        
+        maid.cooking_complete = cooking_valid
+        maid.save()
+        if cooking_valid == True:
+            maid_completed(maid)
+
 
 @receiver(post_save, sender=MaidStatus)
-def maid_status_completed(sender, instance, **kwargs):
-    maid = instance.maid
-    status_valid = True
-    
-    while status_valid == True:
-        for i in sender.values():
-            if not i:
+def maid_status_completed(sender, instance, created, **kwargs):
+    if created == False:
+        maid = instance.maid
+        status_valid = True
+
+        for k,v in instance.__dict__.items():
+            if not v:
                 status_valid = False
-    
-    maid.status_complete = status_valid
-    maid.save()
+        
+        maid.status_complete = status_valid
+        maid.save()
+        if status_valid == True:
+            maid_completed(maid)
