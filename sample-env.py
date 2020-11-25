499cd1208fee6be5a6f7e60389cd21b4f1db4e56
polygonalr <nighttag492@gmail.com>
2020-11-25 12:46:15 +0800

    Update sample-env with fake keys

--- sample-env.py
+++ sample-env.py
@@ -1,6 +1,6 @@
 import os 
 os.environ['POSTGRESQL_URL'] = '<<POSTGRESQL_URL>>'
-os.environ['SOCIAL_AUTH_FACEBOOK_KEY'] = ''
-os.environ['SOCIAL_AUTH_FACEBOOK_SECRET'] = ''
-os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_KEY'] = ''
-os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET'] = ''
+os.environ['SOCIAL_AUTH_FACEBOOK_KEY'] = '000000000000000'
+os.environ['SOCIAL_AUTH_FACEBOOK_SECRET'] = '0123456789abcdef01263456789abcde'
+os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_KEY'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com'
+os.environ['SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET'] = 'xxxxxxxxxxxxxxxxxxxxxxxx'
