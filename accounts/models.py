8c81caefb6b206247e33a954032da1b60ed14d3c
polygonalr <nighttag492@gmail.com>
2020-12-06 20:44:39 +0800

    Add logging for successful and failed logins

--- accounts/models.py
+++ accounts/models.py
@@ -2,14 +2,36 @@
 
 # Imports from django
 from django.db import models
+from django.dispatch import receiver
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.signals import user_logged_in, user_login_failed
 from django.utils.translation import ugettext_lazy as _
 
 # Imports from within the app
 from .managers import CustomUserManager
 
 # Utiliy Classes and Functions
+class AuditEntry(models.Model):
+    action = models.CharField(max_length=64)
+    ip = models.GenericIPAddressField(null=True)
+    username = models.CharField(max_length=256, null=True)
+
+    def __unicode__(self):
+        return '{0} - {1} - {2}'.format(self.action, self.username, self.ip)
+
+    def __str__(self):
+        return '{0} - {1} - {2}'.format(self.action, self.username, self.ip)
+
+@receiver(user_logged_in)
+def user_logged_in_callback(sender, request, user, **kwargs):  
+    ip = request.META.get('REMOTE_ADDR')
+    AuditEntry.objects.create(action='user_logged_in', ip=ip, username=user.email)
+
+@receiver(user_login_failed)
+def user_login_failed_callback(sender, credentials, **kwargs):
+    # TODO Implement IP lock out after failed attempts
+    AuditEntry.objects.create(action='user_login_failed', username=credentials.get('username', None))
 
 # Start of Models
 
