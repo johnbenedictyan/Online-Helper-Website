3a0795252b4d08ec9585eb85cc30e1164d706c3a
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-12-09 01:05:52 +0800

    Minor code refactoring

--- employer_documentation/mixins.py
+++ employer_documentation/mixins.py
@@ -26,8 +26,7 @@ class CheckEmployerExtraInfoBelongsToEmployerMixin(UserPassesTestMixin):
         test_obj = EmployerExtraInfo.objects.get(
             pk=self.kwargs.get('employer_extra_info_pk')
         )
-        if self.get_object().employer_base.pk==self.kwargs.get(
-            'employer_base_pk'):
+        if test_obj.employer_base.pk==self.kwargs.get('employer_base_pk'):
             return True
         else:
             return False
