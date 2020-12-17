4c23fbf6701a56b08cb5a24a15ca858a5ef07d5c
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-12-17 17:23:26 +0800

    Add missing field to EmployerDocServiceAgreement model, make migrations

--- employer_documentation/models.py
+++ employer_documentation/models.py
@@ -597,6 +597,42 @@ class EmployerDocServiceAgreement(models.Model):
         ]
     )
     c5_1_1_failed_deployment_refund = models.PositiveIntegerField()
+    c5_1_2_refund_within_days = models.PositiveIntegerField(
+        # days
+        choices=[
+            (0, "0"),
+            (1, "1"),
+            (2, "2"),
+            (3, "3"),
+            (4, "4"),
+            (5, "5"),
+            (6, "6"),
+            (7, "7"),
+            (8, "8"),
+            (9, "9"),
+            (10, "10"),
+            (11, "11"),
+            (12, "12"),
+            (13, "13"),
+            (14, "14"),
+            (15, "15"),
+            (16, "16"),
+            (17, "17"),
+            (18, "18"),
+            (19, "19"),
+            (20, "20"),
+            (21, "21"),
+            (22, "22"),
+            (23, "23"),
+            (24, "24"),
+            (25, "25"),
+            (26, "26"),
+            (27, "27"),
+            (28, "28"),
+            (29, "29"),
+            (30, "30"),
+        ]
+    )
     c5_1_2_before_fdw_arrives_charge = models.PositiveIntegerField()
     c5_1_2_after_fdw_arrives_charge = models.PositiveIntegerField()
     c5_2_2_can_transfer_refund_within = models.PositiveSmallIntegerField(
