0cbb583c6699661a09ba94feb1aa29b3a392d22e
johnbenedictyan <johnyfx00@gmail.com>
2020-12-23 12:59:20 +0800

    Updated the enquiry form to the universla enquiry form.

--- enquiry/forms.py
+++ enquiry/forms.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 # Imports from foreign installed apps
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
+from crispy_forms.bootstrap import InlineCheckboxes
 
 # Imports from local apps
 
@@ -16,112 +17,161 @@ from crispy_forms.layout import Layout, Submit, Row, Column
 
 # Generic Forms (forms.Form)
 class GeneralEnquiryForm(forms.Form):
-    # FIELDS TO ADD for detailed:
-    # Change main responsibility to responsibilites with a multi checkbox
-    # Add care for pet and gardening in responsibilites
+    # Settings
+
+    ## General
+    NO_PREFERENCE = 'ALL'
+    OTHERS        = 'OTH'
+
+    ## Maid Age
+    MAID_AGE_23_TO_29 = '23-29'
+    MAID_AGE_30_TO_39 = '30-39'
+    MAID_AGE_40_TO_49 = '40-49'
+    MAID_AGE_ABOVE_50 = 'above 50'
+
+    ## Maid Nationality
+    MAID_NATIONALITY_CAMBODIAN  = 'KHM'
+    MAID_NATIONALITY_FILIPINO   = 'PHL'
+    MAID_NATIONALITY_INDIAN     = 'IND'
+    MAID_NATIONALITY_INDONESIAN = 'IDN'
+    MAID_NATIONALITY_MYANMARESE = 'MMR'
+    MAID_NATIONALITY_SRI_LANKAN = 'LKA'
+
+    ## Maid Responsibilites
+    MAID_RESP_GENERAL_HOUSEWORK         = 'GEH'
+    MAID_RESP_COOKING                   = 'COK'
+    MAID_RESP_CARE_FOR_INFANTS_CHILDREN = 'CFI'
+    MAID_RESP_CARE_FOR_ELDERLY          = 'CFE'
+    MAID_RESP_CARE_FOR_DISABLED         = 'CFD'
+    MAID_RESP_CARE_FOR_PETS             = 'CFP'
+    MAID_RESP_GARDENING                 = 'GAR'
     
-    # Change enquiry form to one form, all fields compulsory
+    ## Maid Rest Days
+    MAID_REST_DAY_0 = '0RD'
+    MAID_REST_DAY_1 = '1RD'
+    MAID_REST_DAY_2 = '2RD'
+    MAID_REST_DAY_3 = '3RD'
+    MAID_REST_DAY_4 = '4RD'
+
+    ## Maid Type
+    MAID_TYPE_NO_EXPERIENCE       = 'NEW'
+    MAID_TYPE_TRANSFER            = 'TRA'
+    MAID_TYPE_SG_EXPERIENCE       = 'SGE'
+    MAID_TYPE_OVERSEAS_EXPERIENCE = 'OVE'
+
+    ## Property Type
+    PROPERTY_2_ROOM_HDB               = '2RMHDB'
+    PROPERTY_3_ROOM_HDB               = '3RMHDB'
+    PROPERTY_4_ROOM_HDB               = '4RMHDB'
+    PROPERTY_5_ROOM_HDB               = '5RMHDB'
+    PROPERTY_EXECUTIVE_MAISONETTE_HDB = 'E/MHDB'
+    PROPERTY_CONDOMINIUM              = 'CONDO'
+    PROPERTY_CONDOMINIUM_PENTHOUSE    = 'CONDOP'
+    PROPERTY_TERRACE                  = 'TERRACE'
+    PROPERTY_SEMI_DETACHED            = 'SEMI-D'
+    PROPERTY_BUNGALOW                 = 'BUNGLO'
+    PROPERTY_OTHERS                   = 'OTH'
     
-    # Add property field
-    # HDB: 2,3,4,5 executive/massionnette
-    # condo: condo, penthouse
-    # landed: terrace, semi-d, bungalow, others
-
-    # Age group field
-    # 23-29, 30-39, 40-49, above 50
-
-    # number of rest day per month
-    # 0 rest day per month
-    # 1 rest day per month
-    # 2 rest day per month
-    # 3 rest day per month
-    # 4 rest day per month
-
-    # Number of family members in total residing
-    # No of below 12 residing
-    # No of baby residing
-
     MAID_NATIONALITY_CHOICES = (
-        ('ALL', _('No preference')),
-        ('KHM', _('Cambodian')),
-        ('PHL', _('Filipino')),
-        ('IND', _('Indian')),
-        ('IDN', _('Indonesian')),
-        ('MMR', _('Myanmarese')),
-        ('LKA', _('Sri Lankan')),
-        ('OTH', _('Others'))
+        (NO_PREFERENCE, _('No preference')),
+        (MAID_NATIONALITY_CAMBODIAN, _('Cambodian')),
+        (MAID_NATIONALITY_FILIPINO, _('Filipino')),
+        (MAID_NATIONALITY_INDIAN, _('Indian')),
+        (MAID_NATIONALITY_INDONESIAN, _('Indonesian')),
+        (MAID_NATIONALITY_MYANMARESE, _('Myanmarese')),
+        (MAID_NATIONALITY_SRI_LANKAN, _('Sri Lankan')),
+        (OTHERS, _('Others'))
     )
 
-    MAID_MAIN_RESPONSIBILITY_CHOICES = (
-        ('ALL', _('No preference')),
-        ('GEH', _('General Housework')),
-        ('COK', _('Cooking')),
-        ('CFI', _('Care for Infants/Children')),
-        ('CFE', _('Care for the Elderly')),
-        ('CFD', _('Care for the Disabled'))
+    MAID_RESP_CHOICES = (
+        (NO_PREFERENCE, _('No preference')),
+        (MAID_RESP_GENERAL_HOUSEWORK, _('General Housework')),
+        (MAID_RESP_COOKING, _('Cooking')),
+        (MAID_RESP_CARE_FOR_INFANTS_CHILDREN, _('Care for Infants/Children')),
+        (MAID_RESP_CARE_FOR_ELDERLY, _('Care for the Elderly')),
+        (MAID_RESP_CARE_FOR_DISABLED, _('Care for the Disabled')),
+        (MAID_RESP_CARE_FOR_PETS, _('Care for Pets')),
+        (MAID_RESP_GARDENING, _('Gardening'))
     )
 
     MAID_TYPE_CHOICES = (
-        ('ALL', _('No preference')),
-        ('NEW', _('No Experience')),
-        ('TRA', _('Transfer')),
-        ('SGE', _('Singapore Experience')),
-        ('OVE', _('Overseas Experience'))
+        (NO_PREFERENCE, _('No preference')),
+        (MAID_TYPE_NO_EXPERIENCE, _('No Experience')),
+        (MAID_TYPE_TRANSFER, _('Transfer')),
+        (MAID_TYPE_SG_EXPERIENCE, _('Singapore Experience')),
+        (MAID_TYPE_OVERSEAS_EXPERIENCE, _('Overseas Experience'))
     )
 
     MAID_AGE_CHOICES = (
-        (23, _('23')),
-        (24, _('24')),
-        (25, _('25')),
-        (26, _('26')),
-        (27, _('27')),
-        (28, _('28')),
-        (29, _('29')),
-        (30, _('30')),
-        (31, _('31')),
-        (32, _('32')),
-        (33, _('33')),
-        (34, _('34')),
-        (35, _('35')),
-        (36, _('36')),
-        (37, _('37')),
-        (38, _('38')),
-        (39, _('39')),
-        (40, _('40')),
-        (41, _('41')),
-        (42, _('42')),
-        (43, _('43')),
-        (44, _('44')),
-        (45, _('45')),
-        (46, _('46')),
-        (47, _('47')),
-        (48, _('48')),
-        (49, _('49')),
-        (50, _('50'))
+        (MAID_AGE_23_TO_29, _('23-29')),
+        (MAID_AGE_30_TO_39, _('30-39')),
+        (MAID_AGE_40_TO_49, _('40-49')),
+        (MAID_AGE_ABOVE_50, _('Above 50'))
+    )
+    
+    PROPERTY_CHOICES = (
+        (PROPERTY_2_ROOM_HDB, _('2-Room HDB')),
+        (PROPERTY_3_ROOM_HDB, _('3-Room HDB')),
+        (PROPERTY_4_ROOM_HDB, _('4-Room HDB')),
+        (PROPERTY_5_ROOM_HDB, _('5-Room HDB')),
+        (PROPERTY_EXECUTIVE_MAISONETTE_HDB, _('Executive/Maisonette HDB')),
+        (PROPERTY_CONDOMINIUM, _('Condominium')),
+        (PROPERTY_CONDOMINIUM_PENTHOUSE, _('Condominium Penthouse')),
+        (PROPERTY_TERRACE, _('Terrace')),
+        (PROPERTY_SEMI_DETACHED, _('Semi-Detached')),
+        (PROPERTY_BUNGALOW, _('Bungalow')),
+        (PROPERTY_OTHERS, _('Others')),
+    )
+    MAID_REST_DAY_CHOICES = (
+        (MAID_REST_DAY_0, _('0 Rest Days Per Month')),
+        (MAID_REST_DAY_1, _('1 Rest Days Per Month')),
+        (MAID_REST_DAY_2, _('2 Rest Days Per Month')),
+        (MAID_REST_DAY_3, _('3 Rest Days Per Month')),
+        (MAID_REST_DAY_4, _('4 Rest Days Per Month'))
     )
 
     first_name = forms.CharField(
         label=_('First Name'),
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'First Name'
+            }
+        )
     )
 
     last_name = forms.CharField(
         label=_('Last Name'),
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Last Name'
+            }
+        )
     )
 
     contact_number = forms.CharField(
         label=_('Contact Number'),
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Contact Number'
+            }
+        )
     )
 
     email = forms.EmailField(
         label=_('Email'),
         required=True,
-        max_length=255
+        max_length=255,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Email Address'
+            }
+        )
     )
 
     maid_nationality = forms.ChoiceField(
@@ -130,10 +180,11 @@ class GeneralEnquiryForm(forms.Form):
         choices=MAID_NATIONALITY_CHOICES
     )
 
-    maid_main_responsibility = forms.ChoiceField(
+    maid_responsibility = forms.MultipleChoiceField(
         label=_('Maid\'s main responsibility'),
         required=True,
-        choices=MAID_MAIN_RESPONSIBILITY_CHOICES
+        choices=MAID_RESP_CHOICES,
+        widget=forms.CheckboxSelectMultiple()
     )
 
     maid_type = forms.ChoiceField(
@@ -142,16 +193,46 @@ class GeneralEnquiryForm(forms.Form):
         choices=MAID_TYPE_CHOICES
     )
 
-    maid_min_age = forms.ChoiceField(
-        label=_('Minimum age of Maid'),
+    maid_age_group = forms.ChoiceField(
+        label=_('Maid Age Group'),
         required=True,
         choices=MAID_AGE_CHOICES
     )
 
-    maid_max_age = forms.ChoiceField(
-        label=_('Maximum age of Maid'),
+    rest_days = forms.ChoiceField(
+        label=_('Number of Rest Days'),
         required=True,
-        choices=MAID_AGE_CHOICES
+        choices=MAID_REST_DAY_CHOICES
+    )
+    
+    no_of_family_members = forms.IntegerField(
+        label=_('Number of Family Members'),
+        required=True,
+        widget=forms.NumberInput(
+            attrs={
+                'placeholder': 'Number of Family Members'
+            }
+        )
+    )
+
+    no_of_below_12 = forms.IntegerField(
+        label=_('Number of Children below 12'),
+        required=True,
+        widget=forms.NumberInput(
+            attrs={
+                'placeholder': 'Number of Children below 12'
+            }
+        )
+    )
+
+    no_of_babies = forms.IntegerField(
+        label=_('Number of Babies'),
+        required=True,
+        widget=forms.NumberInput(
+            attrs={
+                'placeholder': 'Number of Babies'
+            }
+        )
     )
 
     remarks = forms.CharField(
@@ -194,26 +275,44 @@ class GeneralEnquiryForm(forms.Form):
             Row(
                 Column(
                     'maid_nationality',
-                    css_class='form-group col-md-4'
-                ),
-                Column(
-                    'maid_main_responsibility',
-                    css_class='form-group col-md-4'
+                    css_class='form-group col-md-3'
                 ),
                 Column(
                     'maid_type',
-                    css_class='form-group col-md-4'
+                    css_class='form-group col-md-3'
+                ),
+                Column(
+                    'maid_age_group',
+                    css_class='form-group col-md-3'
+                ),
+                Column(
+                    'rest_days',
+                    css_class='form-group col-md-3'
                 ),
                 css_class='form-row'
             ),
             Row(
                 Column(
-                    'maid_min_age',
-                    css_class='form-group col-md-6'
+                    'maid_responsibility',
+                    css_class='form-group col-md-4 mt-1'
                 ),
                 Column(
-                    'maid_max_age',
-                    css_class='form-group col-md-6'
+                    Row(
+                        Column(
+                            'no_of_family_members',
+                            css_class='form-group col-6'
+                        ),
+                        Column(
+                            'no_of_below_12',
+                            css_class='form-group col-6'
+                        ),
+                        Column(
+                            'no_of_babies',
+                            css_class='form-group col-6'
+                        ),
+                        css_class='form-row'
+                    ),
+                    css_class='form-group col-md-8'
                 ),
                 css_class='form-row'
             ),
