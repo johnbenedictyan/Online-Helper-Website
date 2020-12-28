9c6a3ca1019af499cd3fb6df0baacabfec0cde3b
johnbenedictyan <johnyfx00@gmail.com>
2020-12-28 12:50:25 +0800

    Removed the area field from the agency branch form and added the code which will automatically assign the branch's area based on its postal code.

--- agency/forms.py
+++ agency/forms.py
@@ -503,7 +503,7 @@ class AgencyEmployeeUpdateForm(forms.ModelForm):
 class AgencyBranchForm(forms.ModelForm):
     class Meta:
         model = AgencyBranch
-        exclude = ['agency']
+        exclude = ['agency', 'area']
         widgets = {
             'main_branch': forms.RadioSelect()
         }
@@ -540,6 +540,38 @@ class AgencyBranchForm(forms.ModelForm):
         return cleaned_data
 
     def save(self, *args, **kwargs):
+        CENTRAL = 'C'
+        NORTH = 'N'
+        NORTH_EAST = 'NE'
+        EAST = 'E'
+        WEST = 'W'
+
+        postal_code_area_dict = {
+            CENTRAL : [
+                '01','02','03','04','05','06','07','08','09','10','14','15',
+                '16','17','18','19','20','21','22','23','24','25','26','27',
+                '28','29','30','31','32','33','34','35','36','37','38','39',
+                '40','41','58','59','77','78'
+            ],
+            NORTH : [
+                '69','70','71','72','73','75','76'
+            ],
+            NORTH_EAST : [
+                '53','54','55','56','57','79','80','82'
+            ],
+            EAST : [
+                '42','43','44','45','46','47','48','49','50','51','52','81'
+            ],
+            WEST : [
+                '11','12','13','60','61','62','63','64','65','66','67','68'
+            ]
+        }
+        cleaned_data = self.cleaned_data
+        postal_code = cleaned_data.get('postal_code')
+        for k,v in postal_code_area_dict.items():
+            if str(postal_code[:2]) in v:
+                self.instance.area = k
+
         self.instance.agency = self.agency
         return super().save(*args, **kwargs)
 
@@ -551,11 +583,7 @@ class AgencyBranchForm(forms.ModelForm):
             Row(
                 Column(
                     'name',
-                    css_class='form-group col-md-6'
-                ),
-                Column(
-                    'main_branch',
-                    css_class='form-group col-md-6'
+                    css_class='form-group col'
                 ),
                 css_class='form-row'
             ),
@@ -576,7 +604,7 @@ class AgencyBranchForm(forms.ModelForm):
                     css_class='form-group col-md-6'
                 ),
                 Column(
-                    'area',
+                    'main_branch',
                     css_class='form-group col-md-6'
                 ),
                 css_class='form-row'
