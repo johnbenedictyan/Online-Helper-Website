9ca8999bcdf0316ea4598794902069a9ee448f0f
johnbenedictyan <johnyfx00@gmail.com>
2020-12-29 14:09:54 +0800

    Changed the enquiry view to a create view since it is now creating an enquiry model. Also implemented the potential employer required mixin to the views.

--- enquiry/views.py
+++ enquiry/views.py
@@ -1,12 +1,14 @@
 # Imports from django
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import FormView, ListView, RedirectView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, RedirectView
+from django.views.generic.edit import CreateView
 
 # Imports from project-wide files
 
 # Imports from foreign installed apps
 from accounts.models import Employer
+from accounts.mixins import PotentialEmployerRequiredMixin
 
 # Imports from local app
 from .forms import EnquiryForm
@@ -15,20 +17,31 @@ from .models import Enquiry
 # Start of Views
 
 # Form Views
-class EnquiryView(LoginRequiredMixin, FormView):
+class EnquiryView(PotentialEmployerRequiredMixin, CreateView):
+    context_object_name = 'enquiry'
     form_class = EnquiryForm
     http_method_names = ['get', 'post']
+    model = Enquiry
     template_name = 'enquiry.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.employer = Employer.objects.get(
+            user = self.request.user
+        )
+        return super().form_valid(form)
 
 # Redirect Views
-class DeactivateEnquiryView(LoginRequiredMixin, RedirectView):
+class DeactivateEnquiryView(PotentialEmployerRequiredMixin, RedirectView):
     http_method_names = ['get']
     pattern_name = None
 
     def get_redirect_url(self, *args, **kwargs):
         try:
             selected_enquiry = Enquiry.objects.get(
-                pk = kwargs.get('pk'),
+                pk = kwargs.get(
+                    self.pk_url_kwarg
+                ),
                 active = True
             )
         except Enquiry.DoesNotExist:
@@ -51,7 +64,7 @@ class DeactivateEnquiryView(LoginRequiredMixin, RedirectView):
         return super().get_redirect_url(*args, **kwargs)
 
 # List Views
-class EnquiryListView(LoginRequiredMixin, ListView):
+class EnquiryListView(PotentialEmployerRequiredMixin, ListView):
     context_object_name = 'enquries'
     http_method_names = ['get']
     model = Enquiry
@@ -59,9 +72,7 @@ class EnquiryListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Enquiry.objects.filter(
-            employer = Employer.objects.get(
-                user = self.request.user
-            ),
+            employer__user = self.request.user,
             active = True
         )
 
