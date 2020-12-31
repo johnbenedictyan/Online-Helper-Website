bee53c39e61e946ee23f8894965107864b23cf92
johnbenedictyan <johnyfx00@gmail.com>
2020-12-31 12:13:45 +0800

    Added the authority groups list which consist of all of the permission groups.

--- onlinemaid/constants.py
+++ onlinemaid/constants.py
@@ -6,6 +6,13 @@ AG_ADMINS = 'Agency Administrators'
 AG_MANAGERS = 'Agency Managers'
 AG_SALES = 'Agency Sales Staff'
 P_EMPLOYERS = 'Potential Employers'
+AUTHORITY_GROUPS = [
+    AG_OWNERS,
+    AG_ADMINS,
+    AG_MANAGERS,
+    AG_SALES,
+    P_EMPLOYERS
+]
 
 def TrueFalseChoices(TrueMessage, FalseMessage):
     return (
