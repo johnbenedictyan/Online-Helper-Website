2ee27550b68b684b76c527b9162b444ceaf271bd
johnbenedictyan <johnbenedictyan@gmail.com>
2021-01-11 22:19:01 +0800

    Added a mini maid filter for the filter form on the home page.

--- maid/filters.py
+++ maid/filters.py
@@ -1,12 +1,40 @@
 # Imports from django
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 # Imports from foreign installed apps
 import django_filters
 
 # Imports from local apps
-from .models import Maid
+from .constants import TypeOfMaidChoices, MaidCountryOfOrigin
+from .models import Maid, MaidResponsibility
 
 # Start of Filters
+class MiniMaidFilter(django_filters.FilterSet):
+    biodata__country_of_origin = django_filters.ChoiceFilter(
+        choices = MaidCountryOfOrigin.choices,
+        empty_label = _('Any'),
+        label=''
+    )
+    maid_type = django_filters.ChoiceFilter(
+        choices = TypeOfMaidChoices.choices,
+        empty_label = _('Any'),
+        label=''
+    )
+    responsibilities = django_filters.ModelMultipleChoiceFilter(
+        queryset=MaidResponsibility.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        label=''
+    )
+    
+    class Meta:
+        model = Maid
+        fields = {
+            'biodata__country_of_origin': ['exact'],
+            'maid_type': ['exact'],
+            'responsibilities': ['exact']
+        }
+        
 class MaidFilter(django_filters.FilterSet):
     # TODO: Add main responsibility and language ability
     class Meta:
@@ -15,5 +43,6 @@ class MaidFilter(django_filters.FilterSet):
             'biodata__country_of_origin': ['exact'],
             'maid_type': ['exact'],
             'biodata__age': ['lt', 'gt'],
-            'family_details__marital_status': ['exact']
+            'family_details__marital_status': ['exact'],
+            'responsibilities': ['exact']
         }
