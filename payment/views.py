0b2f015bc2ad9e62a22e508573ddfb9e6932235f
johnbenedictyan <johnbenedictyan@gmail.com>
2021-01-13 23:58:12 +0800

    Added the checkout success and cancel views as well as initialised the create checkout session view.

--- payment/views.py
+++ payment/views.py
@@ -1,9 +1,13 @@
+# Imports from modules
+import json
+
 # Imports from django
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import JsonResponse
 from django.urls.base import reverse_lazy
-from django.views.generic import ListView
+from django.views.generic import ListView, View
 from django.views.generic.base import RedirectView
 from django.views.generic.detail import DetailView
 
@@ -11,6 +15,7 @@ from django.views.generic.detail import DetailView
 
 # Imports from foreign installed apps
 import stripe
+from stripe.api_resources import line_item, payment_method
 from agency.mixins import AgencyOwnerRequiredMixin, GetAuthorityMixin
 
 # Imports from local app
@@ -19,6 +24,7 @@ from .models import Invoice, Customer
 # Start of Views
 
 # Template Views
+from django.http import JsonResponse
 
 # Redirect Views
 class CustomerPortal(AgencyOwnerRequiredMixin, GetAuthorityMixin,
@@ -30,7 +36,7 @@ class CustomerPortal(AgencyOwnerRequiredMixin, GetAuthorityMixin,
         stripe.api_key = settings.STRIPE_SECRET_KEY
         customer_id = Customer.objects.get(
             agency__pk=self.agency_id
-        ).stripe_customer_id
+        ).pk
         session = stripe.billing_portal.Session.create(
             customer=customer_id,
             return_url=self.request.build_absolute_uri(
@@ -38,6 +44,30 @@ class CustomerPortal(AgencyOwnerRequiredMixin, GetAuthorityMixin,
             ),
         )
         return session.url
+
+class CheckoutSucess(RedirectView):
+    http_method_names = ['get']
+    pattern_name = None
+    
+    def get_redirect_url(self, *args, **kwargs):
+        messages.success(
+            self.request,
+            'Payment Sucessful',
+            extra_tags='sucess'
+        )
+        return super().get_redirect_url()
+    
+class CheckoutCancel(RedirectView):
+    http_method_names = ['get']
+    pattern_name = None
+    
+    def get_redirect_url(self, *args, **kwargs):
+        messages.info(
+            self.request,
+            'Payment Cancelled',
+            extra_tags='info'
+        )
+        return super().get_redirect_url()
     
 # List Views
 class InvoiceList(LoginRequiredMixin, ListView):
@@ -61,3 +91,36 @@ class InvoiceDetail(LoginRequiredMixin, DetailView):
 # Create Views
 
 # Delete Views
+
+# Generic Views
+class CheckoutSession(View):
+    http_method_names = ['post']
+    
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.data)
+        
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                payment_method_types = ['card'],
+                mode = 'subscription',
+                line_items = [
+                    {
+                        'price': 'fake',
+                        'quantity': 123
+                    }
+                ]
+            )
+            return JsonResponse(
+                {
+                    'sessionId': checkout_session['id']
+                }
+            )
+        except Exception as e:
+            return JsonResponse(
+                {
+                    'error': {
+                        'message': str(e)
+                    }
+                },
+                status = 400
+            )
