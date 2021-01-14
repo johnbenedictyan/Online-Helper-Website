9fd216acfe821319428195f0403bce8f8b0119ad
johnbenedictyan <johnbenedictyan@gmail.com>
2021-01-15 00:58:17 +0800

    Registered the payment models.

--- payment/admin.py
+++ payment/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Customer
+from .models import (
+    Customer, SubscriptionProduct, SubscriptionProductImage, 
+    SubscriptionProductPrice
+)
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(SubscriptionProduct)
+admin.site.register(SubscriptionProductImage)
+admin.site.register(SubscriptionProductPrice)
