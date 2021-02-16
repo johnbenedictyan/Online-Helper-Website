ac36397d5a8d49fffedeaeab1e30817dd6139c9d
johnbenedictyan <johnyfx00@gmail.com>
2021-02-16 19:44:55 +0800

    Hotfix because sentry sdk is breaking the server.

--- onlinemaid/settings.py
+++ onlinemaid/settings.py
@@ -17,8 +17,8 @@ from pathlib import Path
 
 # 3rd party libraries
 import dj_database_url
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+# import sentry_sdk
+# from sentry_sdk.integrations.django import DjangoIntegration
 
 # Imports from Django
 from django.contrib.messages import constants as messages
@@ -276,17 +276,17 @@ ENCRYPTION_KEY = os.environ.get('ENCRYPTION_KEY')
 SITE_ID = 1
 
 # Sentry
-SENTRY_DSN = os.environ.get('SENTRY_DSN')
-sentry_sdk.init(
-    dsn=SENTRY_DSN,
-    integrations=[
-        DjangoIntegration(
-            transaction_style='function_name'
-        )
-    ],
-    traces_sample_rate=1.0,
+# SENTRY_DSN = os.environ.get('SENTRY_DSN')
+# sentry_sdk.init(
+#     dsn=SENTRY_DSN,
+#     integrations=[
+#         DjangoIntegration(
+#             transaction_style='function_name'
+#         )
+#     ],
+#     traces_sample_rate=1.0,
 
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
+#     # If you wish to associate users to errors (assuming you are using
+#     # django.contrib.auth) you may enable sending PII data.
+#     send_default_pii=True
+# )
