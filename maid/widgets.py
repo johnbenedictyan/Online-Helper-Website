a524076ea428e8cf5410329bd34dc43c9b2a56fc
johnbenedictyan <johnyfx00@gmail.com>
2021-02-16 12:39:01 +0800

    Gave the custom date input a default placeholder.

--- maid/widgets.py
+++ maid/widgets.py
@@ -2,3 +2,9 @@ from django.forms import DateInput
 
 class CustomDateInput(DateInput):
     template_name='widgets/custom-datepicker.html'
+
+    def __init__(self, attrs=None):
+        attrs = {
+            'placeholder': 'Select Date'
+        }
+        super().__init__(attrs)
