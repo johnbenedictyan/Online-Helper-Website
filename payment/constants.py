97a48aaa5af82128635665ebfc5cd85b2b3a35ba
johnbenedictyan <johnbenedictyan@gmail.com>
2021-03-06 00:02:13 +0800

    Added the subscription choices into a constants file.

---
+++ payment/constants.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class SubscriptionStatusChoices(models.TextChoices):
+    INCOMPLETE = 'INCOMPLETE', _('Incomplete')
+    INCOMPLETE_EXPIRED = 'INCOMPLETE_EXPIRED', _('Incomplete (Expired)')
+    TRIALING = 'TRIALING', _('Trialing')
+    ACTIVE = 'ACTIVE', _('Active')
+    PAST_DUE = 'PAST_DUE', _('Past Due')
+    CANCELED = 'CANCELED', _('Canceled')
+    UNPAID = 'UNPAID', _('Unpaid')
