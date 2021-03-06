df82656064abcf59abbed97264ba6987568b573e
johnbenedictyan <johnbenedictyan@gmail.com>
2021-03-06 11:42:18 +0800

    Added amount of employee check.

--- agency/views.py
+++ agency/views.py
@@ -148,10 +148,19 @@ class AgencyEmployeeCreate(AgencyOwnerRequiredMixin, SuccessMessageMixin,
         return kwargs
 
     def form_valid(self, form):
-        form.instance.agency = Agency.objects.get(
+        agency = Agency.objects.get(
             pk = self.request.user.agency_owner.agency.pk
         )
-        return super().form_valid(form)
+        form.instance.agency = agency
+        if agency.amount_of_employees < agency.amount_of_employees_allowed:
+            return super().form_valid(form)
+        else:
+            messages.warning(
+                self.request,
+                'You have reached the limit of employee accounts',
+                extra_tags='error'
+            )
+            return super().form_invalid(form)
 
 class AgencyPlanCreate(AgencyOwnerRequiredMixin, SuccessMessageMixin,
                        CreateView):
