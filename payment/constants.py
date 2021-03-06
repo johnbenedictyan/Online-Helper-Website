61d45f888fa8c539520d0a1ecfa4ce1ee44abdf3
johnbenedictyan <johnbenedictyan@gmail.com>
2021-03-06 11:40:05 +0800

    Changed the constants.

--- payment/constants.py
+++ payment/constants.py
@@ -10,9 +10,41 @@ class SubscriptionStatusChoices(models.TextChoices):
     CANCELED = 'CANCELED', _('Canceled')
     UNPAID = 'UNPAID', _('Unpaid')
     
-SubscriptionBiodataLimitChoicesMap = {
-    "prod_IlJqpv40JRgCa6": 200,
-    "prod_IlJl5XiqUt2O3Q": 200,
-    "prod_IlJiwIHP2LXuRI": 100,
-    "prod_IlJVbv4ixfNtNg": 100,
+SubscriptionLimitMap = {
+    "prod_IlJqpv40JRgCa6": {
+        "name": "Premium Platform & CRM Plan",
+        "biodata": 200,
+        "documents": 200,
+        "employee_accounts": 12
+    },
+    "prod_IlJiwIHP2LXuRI": {
+        "name": "Standard Platform & CRM Plan",
+        "biodata": 100,
+        "documents": 100,
+        "employee_accounts": 8
+    },
+    "prod_IlJnULpKUetaRc": {
+        "name": "Premium CRM Plan",
+        "biodata": 0,
+        "documents": 200,
+        "employee_accounts": 12
+    },
+    "prod_IlJf58jk8siXvV": {
+        "name": "Standard CRM Plan",
+        "biodata": 0,
+        "documents": 100,
+        "employee_accounts": 8
+    },
+    "prod_IlJl5XiqUt2O3Q": {
+        "name": "Premium Platform Plan",
+        "biodata": 200,
+        "documents": 0,
+        "employee_accounts": 12
+    },
+    "prod_IlJVbv4ixfNtNg": {
+        "name": "Standard Platform Plan",
+        "biodata": 100,
+        "documents": 0,
+        "employee_accounts": 8
+    }
 }
