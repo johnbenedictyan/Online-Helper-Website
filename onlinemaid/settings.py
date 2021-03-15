b1ab1ae89c553f70bec10d91c2a5c03a8aaed5ba
johnbenedictyan <johnyfx00@gmail.com>
2021-03-15 14:11:27 +0800

    Added oauth context processor.

--- onlinemaid/settings.py
+++ onlinemaid/settings.py
@@ -102,6 +102,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
                 'onlinemaid.context_processors.authority',
                 'onlinemaid.context_processors.cartcount',
                 'onlinemaid.context_processors.enquiry_form'
