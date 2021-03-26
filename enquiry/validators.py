26bbf57a0bd8bfb67cb5904c229a96ab06c21a33
johnbenedictyan <johnyfx00@gmail.com>
2021-03-26 20:01:10 +0800

    Added the custom validators to detect obscene language and links in the enquiry remarks.

---
+++ enquiry/validators.py
@@ -0,0 +1,19 @@
+import re
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from profanity_check import predict
+
+def validate_obscene_language(value):
+    if predict([value] == 1):
+        raise ValidationError(
+            _('%(value)s contains obscene language'),
+            params={'value': value},
+        )
+
+def validate_links(value):
+    url_regex = "(?P<url>https?://[^\s]+)"
+    if re.search(url_regex, value).group('url'):
+        raise ValidationError(
+            _('%(value)s contains links'),
+            params={'value': value},
+        )
