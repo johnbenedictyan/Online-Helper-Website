9f253d5015e00f27a176dd133d87e5378d07eb1a
andrewsui <67029820+andrewsui@users.noreply.github.com>
2021-04-06 18:25:30 +0800

    Setup IncomeChoices constants class for monthly income

--- employer_documentation/constants.py
+++ employer_documentation/constants.py
@@ -1,6 +1,22 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+class IncomeChoices(models.IntegerChoices):
+    INCOME_0 = 0, _("Below $2,000")
+    INCOME_1 = 1, _("$2,000 to $2,499")
+    INCOME_2 = 2, _("$2,500 to $2,999")
+    INCOME_3 = 3, _("$3,000 to $3,499")
+    INCOME_4 = 4, _("$3,500 to $3,999")
+    INCOME_5 = 5, _("$4,000 to $4,999")
+    INCOME_6 = 6, _("$5,000 to $5,999")
+    INCOME_7 = 7, _("$6,000 to $7,999")
+    INCOME_8 = 8, _("$8,000 to $9,999")
+    INCOME_9 = 9, _("$10,000 to $12,499")
+    INCOME_10 = 10, _("$12,500 to $14,999")
+    INCOME_11 = 11, _("$15,000 to $19,999")
+    INCOME_12 = 12, _("$20,000 to $24,999")
+    INCOME_13 = 13, _("$25,000 and above")
+
 class RelationshipChoices(models.TextChoices):
     SON = 'SON', _('Son')
     DAUGHTER = 'DAUGHTER', _('Daughter')
