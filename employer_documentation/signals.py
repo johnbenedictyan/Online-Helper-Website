e98b075be7ab26f831268f9b9649139b6e745b19
andrew <67029820+andrewsui@users.noreply.github.com>
2021-05-31 13:53:54 +0800

    Comment out employer doc app signals

--- employer_documentation/signals.py
+++ employer_documentation/signals.py
@@ -1,36 +1,32 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
-from . import models
+# from . import models
 
-@receiver(post_save, sender=models.EmployerDoc)
-def employer_doc_post_save(sender, instance, created, **kwargs):
-    if created:
-        # If SQL INSERT, create models.EmployerDocSig, models.EmployerDocMaidStatus,
-        # JobOrder, models.PdfArchive instances.
-        models.EmployerDocSig.objects.create(employer_doc=instance)
-        models.EmployerDocMaidStatus.objects.create(employer_doc=instance)
-        # models.PdfArchive.objects.create(employer_doc=instance)
-    else:
-        # If SQL UPDATE, create models.EmployerDocSig, models.EmployerDocMaidStatus,
-        # JobOrder, models.PdfArchive instances if they don't exist.
-        if not hasattr(instance, 'rn_signatures_ed'):
-            models.EmployerDocSig.objects.create(employer_doc=instance)
-        if not hasattr(instance, 'rn_maidstatus_ed'):
-            models.EmployerDocMaidStatus.objects.create(employer_doc=instance)
-        if not hasattr(instance, 'rn_joborder_ed'):
-            JobOrder.objects.create(employer_doc=instance)
-        # if not hasattr(instance, 'rn_models.PdfArchive_ed'):
-        #     models.PdfArchive.objects.create(employer_doc=instance)
+# @receiver(post_save, sender=models.EmployerDoc)
+# def employer_doc_post_save(sender, instance, created, **kwargs):
+#     if created:
+#         # If SQL INSERT, create ... instances.
+#         pass
+#         # models.EmployerDocSig.objects.create(employer_doc=instance)
+#         # models.EmployerDocMaidStatus.objects.create(employer_doc=instance)
+#         # models.PdfArchive.objects.create(employer_doc=instance)
+#     else:
+#         # If SQL UPDATE, create ... instances if they don't exist.
+#         pass
+#         # if not hasattr(instance, 'rn_signatures_ed'):
+#         #     models.EmployerDocSig.objects.create(employer_doc=instance)
+#         # if not hasattr(instance, 'rn_maidstatus_ed'):
+#         #     models.EmployerDocMaidStatus.objects.create(employer_doc=instance)
         
-        # If SQL UPDATE, reset all e-signatures in models.EmployerDocSig instance
-        doc_sig_obj = instance.rn_signatures_ed
-        doc_sig_obj.employer_signature = None
-        doc_sig_obj.fdw_signature = None
-        doc_sig_obj.agency_staff_signature = None
-        doc_sig_obj.spouse_signature = None
-        doc_sig_obj.sponsor_signature = None
-        doc_sig_obj.employer_witness_signature = None
-        doc_sig_obj.fdw_witness_signature = None
-        doc_sig_obj.agency_staff_witness_signature = None
-        doc_sig_obj.save()
+#         # If SQL UPDATE, reset all e-signatures in models.EmployerDocSig instance
+#         # doc_sig_obj = instance.rn_signatures_ed
+#         # doc_sig_obj.employer_signature = None
+#         # doc_sig_obj.fdw_signature = None
+#         # doc_sig_obj.agency_staff_signature = None
+#         # doc_sig_obj.spouse_signature = None
+#         # doc_sig_obj.sponsor_signature = None
+#         # doc_sig_obj.employer_witness_signature = None
+#         # doc_sig_obj.fdw_witness_signature = None
+#         # doc_sig_obj.agency_staff_witness_signature = None
+#         # doc_sig_obj.save()
