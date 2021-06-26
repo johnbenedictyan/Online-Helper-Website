e6297af31276af2ca05f9b4a18b75f231c9b4565
John Benedict <johnbenedictyan@gmail.com>
2021-06-26 22:31:07 +0800

    Reinstated the last login functionality.

--- accounts/signals.py
+++ accounts/signals.py
@@ -11,17 +11,7 @@ from django.dispatch import receiver
 from .models import User
 
 # Start of Signals
-# @receiver(user_logged_in)
-# def got_online(sender, user, request, **kwargs):    
-#     user.is_online = True
-#     user.save()
-
-# @receiver(user_logged_out)
-# def got_offline(sender, user, request, **kwargs):   
-#     user.is_online = False
-#     user.save()
-
-# @receiver(user_logged_in)
-# def set_last_login(sender, user, request, **kwargs):    
-#     user.last_login = timezone.now()
-#     user.save()
+@receiver(user_logged_in)
+def set_last_login(sender, user, request, **kwargs):    
+    user.last_login = timezone.now()
+    user.save()
