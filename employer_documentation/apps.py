f311930aa15fc49771b2b9d21ebec0fd8a459b82
andrew <67029820+andrewsui@users.noreply.github.com>
2021-07-02 18:01:16 +0800

    Remove signal ready() method from EmployerDocumentationConfig AppConfig

--- employer_documentation/apps.py
+++ employer_documentation/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 
 class EmployerDocumentationConfig(AppConfig):
     name = 'employer_documentation'
-
-    def ready(self):
-        import employer_documentation.signals
