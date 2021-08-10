2cb51b8544be42fe71e27ac052a84ca995b9d661
John Benedict <johnbenedictyan@gmail.com>
2021-08-10 11:53:52 +0800

    Code formatting.'

--- payment/admin.py
+++ payment/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (
-    Customer, SubscriptionProduct, SubscriptionProductImage, 
+    Customer, SubscriptionProduct, SubscriptionProductImage,
     SubscriptionProductPrice, Subscription
 )
 
