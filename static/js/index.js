1736e860e18c76f3ae24983a1939f8530a3ab832
John Benedict <johnbenedictyan@gmail.com>
2021-09-02 21:19:57 +0800

    Updates.

--- static/js/index.js
+++ static/js/index.js
@@ -115,38 +115,7 @@ const displayContent = function(res) {
     };
     featuredMaidsContent += `</div>`;
     $('#featured-maids-wrapper').html(featuredMaidsContent);
-    $('.maid-carousel').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+    maidCarousel();
 }
 
 function populateFeaturedMaids(nationality='ANY') {
