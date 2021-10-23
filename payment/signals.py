eec0f1330eb0ba2677a060cfb884b620b369239c
John Benedict <johnbenedictyan@gmail.com>
2021-10-23 10:33:55 +0800

    Updates.

--- payment/signals.py
+++ payment/signals.py
@@ -10,31 +10,30 @@ from .models import Subscription
 @receiver(post_save, sender=Subscription)
 def provision_subscription(sender, instance, created, **kwargs):
     agency = instance.customer.agency
-    product = instance.product
-    if instance.status == SubscriptionStatusChoices.ACTIVE:
-        if SubscriptionLimitMap[product.pk]['type'] == 'plan':
-            agency.amount_of_biodata_allowed = SubscriptionLimitMap[
-                product.pk
-            ]['biodata']
-            agency.amount_of_documents_allowed = SubscriptionLimitMap[
-                product.pk
-            ]['documents']
-            agency.amount_of_employees_allowed = SubscriptionLimitMap[
-                product.pk
-            ]['employee_accounts']
-        elif (
-            SubscriptionLimitMap[
-                product.pk
-            ]['name'] == 'Featured Maid Advertisement'
-        ):
-            amt_feat_bio_allowed = Subscription.objects.filter(
-                agency=agency,
-                product=product,
-                end_date_gt=timezone.now()
-            ).count()
-            agency.amount_of_featured_biodata_allowed = amt_feat_bio_allowed
+    # if instance.status == SubscriptionStatusChoices.ACTIVE:
+    #     if SubscriptionLimitMap[instance.stripe_id]['type'] == 'plan':
+    #         agency.amount_of_biodata_allowed = SubscriptionLimitMap[
+    #             instance.stripe_id
+    #         ]['biodata']
+    #         agency.amount_of_documents_allowed = SubscriptionLimitMap[
+    #             instance.stripe_id
+    #         ]['documents']
+    #         agency.amount_of_employees_allowed = SubscriptionLimitMap[
+    #             instance.stripe_id
+    #         ]['employee_accounts']
+    #     elif (
+    #         SubscriptionLimitMap[
+    #             instance.stripe_id
+    #         ]['name'] == 'Featured Maid Advertisement'
+    #     ):
+    #         # amt_feat_bio_allowed = Subscription.objects.filter(
+    #         #     agency=agency,
+    #         #     product=product,
+    #         #     end_date_gt=timezone.now()
+    #         # ).count()
+    #         agency.amount_of_featured_biodata_allowed = amt_feat_bio_allowed
 
-    elif instance.status == SubscriptionStatusChoices.CANCELED:
-        agency.active = False
+    # elif instance.status == SubscriptionStatusChoices.CANCELED:
+    #     agency.active = False
 
-    agency.save()
+    # agency.save()
