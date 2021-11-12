6afd1e88a92901a30582f6e2b3da1992dae67c15
John Benedict <johnbenedictyan@gmail.com>
2021-11-12 16:41:52 +0800

    Updates to the api.

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,8 @@
-from maid.models import Maid, MaidLanguage, MaidResponsibility
-from rest_framework.serializers import ModelSerializer, CharField
+from maid.models import (Maid, MaidCooking, MaidDisabledCare, MaidElderlyCare,
+                         MaidGeneralHousework, MaidInfantChildCare,
+                         MaidLanguage, MaidLanguageProficiency,
+                         MaidResponsibility)
+from rest_framework.serializers import CharField, ModelSerializer
 
 
 class MaidLanguageSerializer(ModelSerializer):
@@ -18,6 +21,46 @@ class MaidResponsibilitySerializer(ModelSerializer):
         fields = ['name']
 
 
+class MaidCookingSerializer(ModelSerializer):
+    class Meta:
+        model = MaidCooking
+        fields = ['__all__']
+
+
+class MaidDisabledCareSerializer(ModelSerializer):
+    class Meta:
+        model = MaidDisabledCare
+        fields = ['__all__']
+
+
+class MaidElderlyCareSerializer(ModelSerializer):
+
+    class Meta:
+        model = MaidElderlyCare
+        fields = ['__all__']
+
+
+class MaidGeneralHouseworkSerializer(ModelSerializer):
+
+    class Meta:
+        model = MaidGeneralHousework
+        fields = ['__all__']
+
+
+class MaidInfantChildCareSerializer(ModelSerializer):
+
+    class Meta:
+        model = MaidInfantChildCare
+        fields = ['__all__']
+
+
+class MaidLanguageProficiencySerializer(ModelSerializer):
+
+    class Meta:
+        model = MaidLanguageProficiency
+        fields = ['__all__']
+
+
 class MaidSerializer(ModelSerializer):
     maid_type = CharField(source='get_maid_type_display')
     marital_status = CharField(source='get_marital_status_display')
@@ -26,6 +69,11 @@ class MaidSerializer(ModelSerializer):
     education_level = CharField(source='get_education_level_display')
     languages = MaidLanguageSerializer(read_only=True, many=True)
     responsibilities = MaidResponsibilitySerializer(read_only=True, many=True)
+    infant_child_care = MaidInfantChildCareSerializer(read_only=True)
+    elderly_care = MaidElderlyCareSerializer(read_only=True)
+    disabled_care = MaidDisabledCareSerializer(read_only=True)
+    general_housework = MaidGeneralHouseworkSerializer(read_only=True)
+    cooking = MaidCookingSerializer(read_only=True)
 
     class Meta:
         model = Maid
@@ -36,5 +84,6 @@ class MaidSerializer(ModelSerializer):
             'expected_days_off', 'date_of_birth', 'height', 'weight',
             'place_of_birth', 'address_1', 'address_2', 'repatriation_airport',
             'religion', 'contact_number', 'education_level', 'about_me',
-            'email', 'languages', 'responsibilities'
+            'email', 'languages', 'responsibilities', 'infant_child_care',
+            'elderly_care', 'disabled_care', 'general_housework', 'cooking'
         ]
